5b4805a457aaa5f054628802dd0187df5627ffbf
woody <woody38921218@gmail.com>
2024-03-05 08:40:39 -0800

    feat: REACT_APP_LIST_APPLICATION_URL success & redux save data

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -7,6 +7,9 @@ import StepLabel from "@mui/material/StepLabel";
 import ApplicationItem from "../../components/ApplicationItem";
 // import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchApplications } from "../../redux/applicationsSlice";
 const steps = [
   "Select an application to choose training pipeline",
   "Select a pipeline to choose model",
@@ -26,8 +29,11 @@ const fakeData = [
     application_name: "Application 2",
   },
   ...Array.from({ length: 10 }).map((_, index) => ({
-    application_uid: `UID${index + 457}`, 
-    application_created_time: `2023-${String(index + 3).padStart(2, '0')}-01T00:00:00Z`,
+    application_uid: `UID${index + 457}`,
+    application_created_time: `2023-${String(index + 3).padStart(
+      2,
+      "0"
+    )}-01T00:00:00Z`,
     application_description: `Description for Application ${index + 3}`,
     application_name: `Application ${index + 3}`,
   })),
@@ -35,15 +41,24 @@ const fakeData = [
 
 const transformedData = {
   result: {
-    register: fakeData.slice(0, 6), 
-    unregister: fakeData.slice(6, 12), 
+    register: fakeData.slice(0, 6),
+    unregister: fakeData.slice(6, 12),
   },
 };
 const Dashboard = () => {
   // const theme = useTheme();
   // const colors = tokens(theme.palette.mode);
-  const registeredApps = transformedData.result.register.slice(0, 5);
-  const unregisteredApps = transformedData.result.unregister.slice(0, 5);
+
+  const dispatch = useDispatch();
+  const {
+    result: { register, unregister },
+    status,
+  } = useSelector((state) => state.applications);
+  useEffect(() => {
+    dispatch(fetchApplications());
+  }, [dispatch]);
+  // const registeredApps = transformedData.result.register.slice(0, 5);
+  // const unregisteredApps = transformedData.result.unregister.slice(0, 5);
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
@@ -58,23 +73,35 @@ const Dashboard = () => {
           </Stepper>
         </Box>
       </Box>
-              <Typography variant="h6" color="primary" gutterBottom>
-          Registered Applications
-        </Typography>
-      <Box mt={2} sx={{ minHeight: '200px', maxHeight: '300px', overflowY: 'auto' }}>
-
-        {registeredApps.length > 0 ? registeredApps.map((app, index) => (
-          <ApplicationItem key={index} app={app} />
-        )) : <Typography>No registered applications</Typography>}
-      </Box>
-      <Typography variant="h6" color="primary" gutterBottom sx={{mt:2}}>
-          Unregistered Applications
+      <Typography variant="h6" color="primary" gutterBottom>
+        Registered Applications
       </Typography>
-      <Box mt={2} sx={{ minHeight: '200px', maxHeight: '300px', overflowY: 'auto' }}>
-
-        {unregisteredApps.length > 0 ? unregisteredApps.map((app, index) => (
-          <ApplicationItem key={index} app={app} />
-        )) : <Typography>No unregistered applications</Typography>}
+      <Box
+        mt={2}
+        sx={{ minHeight: "200px", maxHeight: "300px", overflowY: "auto" }}
+      >
+        {register.length > 0 ? (
+          register.map((app, index) => (
+            <ApplicationItem key={index} app={app} />
+          ))
+        ) : (
+          <Typography>No registered applications</Typography>
+        )}
+      </Box>
+      <Typography variant="h6" color="primary" gutterBottom sx={{ mt: 2 }}>
+        Unregistered Applications
+      </Typography>
+      <Box
+        mt={2}
+        sx={{ minHeight: "200px", maxHeight: "300px", overflowY: "auto" }}
+      >
+        {unregister.length > 0 ? (
+          unregister.map((app, index) => (
+            <ApplicationItem key={index} app={app} />
+          ))
+        ) : (
+          <Typography>No unregistered applications</Typography>
+        )}
       </Box>
     </Box>
   );
